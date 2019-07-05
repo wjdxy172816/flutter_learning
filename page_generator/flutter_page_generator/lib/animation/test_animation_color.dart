9ea4301ef8bac0b45bb13e9fd479e30614fe6c36
duxingyu <wjdxy172816@163.com>
2019-07-05 17:00:17 +0800

    添加颜色渐变和平移的动画效果.

---
+++ page_generator/flutter_page_generator/lib/animation/test_animation_color.dart
@@ -0,0 +1,65 @@
+
+
+import 'package:flutter/material.dart';
+
+void main()=>runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin{
+
+  Animation<Color> _animation;
+  AnimationController _controller;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 200,
+          height: 200,
+          color: _animation.value,
+          child: FlutterLogo(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =AnimationController(vsync: this,duration:const Duration(seconds: 2));
+    _animation =ColorTween(begin: Color(0xffffffff),end:Color(0xffff0000))
+        .animate(_controller)
+    ..addListener((){
+      //涉及到动画时,一定要添加addListener方法,并且方法中要调用setState方法对值进行更新,否则的话会一直不动的.
+      setState(() {});
+    })
+    ..addStatusListener((status){
+      if(status ==AnimationStatus.completed){
+        _controller.reverse();
+      }else if(status ==AnimationStatus.dismissed){
+        _controller.forward();
+      }
+    });
+    _controller.forward();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
---
+++ page_generator/flutter_page_generator/lib/animation/test_animation_widget.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+/**
+ * 必须要实现Tricker,否则的话,昂,暂时没搞懂
+ * */
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  Animation<double> animation;
+
+  AnimationController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    //获取屏幕宽度
+    var screenWidth =MediaQuery.of(context).size.width;
+    return Scaffold(
+      body:AnimatedBuilder(animation: controller, builder: (context,child){
+        return Transform(
+          //表示转场动画只有从屏幕的左侧中间位置(因为y轴始终为0)移动到屏幕的中间(因为animation第一次为0,再到1)
+          transform: Matrix4.translationValues(screenWidth*animation.value,0.0,0.0),
+          child: Center(
+            child: Container(
+              width: 200,
+              height: 200,
+              child: FlutterLogo() ,
+            ),
+          ),
+        );
+      }),
+    );
+  }
+
+  void myListener(status) async{
+    if(status ==AnimationStatus.completed){
+      await Future.delayed(Duration(seconds: 2));//暂停2秒钟
+      //当监听到运行到结束时,要移除进场动画设置的监听,否则的话会导致出场动画被重复触发.
+      controller.removeStatusListener(myListener);
+      controller.reset();
+      //将进场动画初始化为出场动画
+      animation =Tween(begin: 0.0,end:1.0)
+          .animate(CurvedAnimation(parent: controller, curve: Curves.fastOutSlowIn));
+      //开始动画
+      controller.forward();
+    }
+  }
+  @override
+  void initState() {
+    super.initState();
+    //初始化动画控制器
+    controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+
+    //初始化进场动画,从坐标到屏幕中间.
+    animation = Tween(begin: -1.0, end: 0.0).animate(controller)
+      ..addListener(() {
+        setState(() {
+          print(animation.value); //打印插值
+        });
+      })
+      ..addStatusListener(myListener);
+    //执行动画
+    controller.forward();
+  }
+
+  @override
+  void dispose() {
+    //持续的动画会导致内存泄漏,所以需要手动释放,根据文档,手动释放资源必须放在super.dispose()之前.
+    controller.dispose();
+    super.dispose();
+  }
+}
